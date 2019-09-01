b3a0383d486c3e718b972166c43cb59ffc2a0104
Christopher Biscardi <chris@christopherbiscardi.com>
2019-09-01 02:15:21 -0700

    add another wait for network to finish

--- packages/gatsby-plugin-printer/run-screenshots.js
+++ packages/gatsby-plugin-printer/run-screenshots.js
@@ -3,6 +3,36 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 const debug = require("debug")("gatsby-plugin-printer:run-screenshots");
 
+function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
+  page.on("request", onRequestStarted);
+  page.on("requestfinished", onRequestFinished);
+  page.on("requestfailed", onRequestFinished);
+
+  let inflight = 0;
+  let fulfill;
+  let promise = new Promise(x => (fulfill = x));
+  let timeoutId = setTimeout(onTimeoutDone, timeout);
+  return promise;
+
+  function onTimeoutDone() {
+    page.removeListener("request", onRequestStarted);
+    page.removeListener("requestfinished", onRequestFinished);
+    page.removeListener("requestfailed", onRequestFinished);
+    fulfill();
+  }
+
+  function onRequestStarted() {
+    ++inflight;
+    if (inflight > maxInflightRequests) clearTimeout(timeoutId);
+  }
+
+  function onRequestFinished() {
+    if (inflight === 0) return;
+    --inflight;
+    if (inflight === maxInflightRequests)
+      timeoutId = setTimeout(onTimeoutDone, timeout);
+  }
+}
 const runScreenshots = async ({ data, code }, puppeteerLaunchOptions = {}) => {
   const browser = await puppeteer.launch(puppeteerLaunchOptions);
   const page = await browser.newPage();
@@ -86,6 +116,9 @@ const runScreenshots = async ({ data, code }, puppeteerLaunchOptions = {}) => {
       );
     })
   );
+
+  // one day we might be able to use page.waitFor
+  await waitForNetworkIdle(page, 500);
 
   const titlePromises = data.map(({ id, fileName, outputDir }) =>
     screenshotDOMElement({
